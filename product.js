0d3541f78a78304cb54f7cfa94a997e2d79c2172
SiffKL <Siff0407@stud.kea.dk>
2023-02-17 12:02:36 +0100

    footer

--- product.js
+++ product.js
@@ -1,26 +1,24 @@
-/*const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get("id");
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("_id");
 
-fetch("https://kea-alt-del.dk/t7/api/products/" + id)
+fetch("https://sejammd-d3cb.restdb.io/rest/jewellery/" + id)
   .then((response) => response.json())
   .then((data) => showProduct(data));
 
 function showProduct(product) {
   console.log(product);
-  document.querySelector(".productsiteinfo h1").textContent =
-    product.productdisplayname;
+  document.querySelector("h1").textContent = product.productname;
 
   document.querySelector(".gender").textContent = product.gender;
 
   document.querySelector(".price").textContent = product.price;
 
-  document.querySelector(".oneproductsection .sale2").textContent =
-    product.discount;
+  document.querySelector(".discounted").textContent = product.discount;
 
   document.querySelector(
     ".productimg"
   ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
-}*/
+}
 
 // ______________ Product information ___________________
 
