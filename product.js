e81c33d051a037c3d432039f73e1cb7bb789fd4e
SiffKL <Siff0407@stud.kea.dk>
2023-02-17 09:29:13 +0100

    product ja

---
+++ product.js
@@ -0,0 +1,44 @@
+/*const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+
+fetch("https://kea-alt-del.dk/t7/api/products/" + id)
+  .then((response) => response.json())
+  .then((data) => showProduct(data));
+
+function showProduct(product) {
+  console.log(product);
+  document.querySelector(".productsiteinfo h1").textContent =
+    product.productdisplayname;
+
+  document.querySelector(".gender").textContent = product.gender;
+
+  document.querySelector(".price").textContent = product.price;
+
+  document.querySelector(".oneproductsection .sale2").textContent =
+    product.discount;
+
+  document.querySelector(
+    ".productimg"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+}*/
+
+// ______________ Product information ___________________
+
+// https://sejammd-d3cb.restdb.io/rest/jewellery/
+
+[
+  {
+    _id: "63e9fcc1aa86075000050c70",
+    image: "63e9fca3aa86075000050c6c",
+    material: "Sterling Silver",
+    price: 465,
+    productname: "Simple Circle Ring",
+    category: "Ring",
+    discount: 0,
+    gender: "Women",
+    soldout: false,
+    brandname: "SEJA",
+  },
+];
+
+// __________________________________________________
