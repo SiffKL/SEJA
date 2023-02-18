b4f55f10660325897d1be80d88e491a952f4ae24
SiffKL <Siff0407@stud.kea.dk>
2023-02-18 12:02:05 +0100

    dynamisk product site

--- product.js
+++ product.js
@@ -2,33 +2,30 @@ console.log("product.js");
 
 // http://127.0.0.1:5501/product.html?_id=63e9fcc1aa86075000050c70
 
-const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get("_id");
+// const urlParams = new URLSearchParams(window.location.search);
+// const id = urlParams.get("_id");
 
-// fetch("https://sejammd-d3cb.restdb.io/rest/jewellery/" + id)
+// fetch("https://sejammd-d3cb.restdb.io/rest/jewellery/" + id, {
+//   method: "get",
+//   headers: {
+//     "x-apikey": "63ef83b8478852088da683ec",
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((data) => showProduct(data));
 
-fetch("https://sejammd-d3cb.restdb.io/rest/jewellery/" + id, {
-  method: "get",
-  headers: {
-    "x-apikey": "63ef83b8478852088da683ec",
-  },
-})
-  .then((response) => response.json())
-  .then((data) => showProduct(data));
+// function showProduct(product) {
+//   console.log(product);
+//   document.querySelector("h1").textContent = product.productname;
 
-function showProduct(product) {
-  console.log(product);
-  document.querySelector("h1").textContent = product.productname;
+//   document.querySelector(".gender").textContent = product.gender;
 
-  document.querySelector(".gender").textContent = product.gender;
+//   document.querySelector(".price").textContent = product.price + "DKK";
 
-  document.querySelector(".price").textContent = product.price + "DKK";
+//   document.querySelector(".material").textContent = product.material;
 
-  document.querySelector(".discounted").textContent =
-    "-" + product.discount + "%";
-
-  document.querySelector(".productimg").src = `img/${product.image}`;
-}
+//   document.querySelector(".productimg").src = `img/${product.image}`;
+// }
 
 // ______________ Product information ___________________
 
@@ -50,3 +47,14 @@ function showProduct(product) {
 ];
 
 // __________________________________________________
+
+//udsolgt produkt, dette i productslist? husk en overlay styly i css
+// if (product.soldout) {
+//   copy.querySelector("section").classList.add("overlay");
+//   // copy.querySelector(".soldout").classList.add(".soldoutp");
+// } else {
+//   copy.querySelector(".soldout").remove();
+// }
+
+// document.querySelector(".div2 .discounted").textContent =
+//   "-" + product.discount + "%";
